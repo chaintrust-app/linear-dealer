54a6ba62f689aa7b7402d53b4a38bc659199a5ea
AERG <anthony@chaintrust.io>
2022-01-08 18:47:04 +0100

    Test env var

--- dist/index.js
+++ dist/index.js
@@ -8469,25 +8469,26 @@ var __webpack_exports__ = {};
 // This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
 const { LinearClient } = __nccwpck_require__(6570)
-const linear = new LinearClient({
+/*const linear = new LinearClient({
     apiKey: "lin_api_2IczXFvoXNVm9LeXPNyo7DcQOtOaZ7eusoQuT2OO"
-});
+});*/
 const core = __nccwpck_require__(9678);
 const github = __nccwpck_require__(4425);
 
 async function getMyIssues() {
-    const issues = await linear.issues()
-    console.log(issues.issueSearch("CHA-921"))
+    /*const issues = await linear.issues()
+    console.log(issues.issueSearch("CHA-921"))*/
     console.log(process.env.LINEAR_API_KEY)
+    console.log(process.env)
 }
 
 try {
     getMyIssues()
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //const time = (new Date()).toTimeString();
+    //core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    console.log(`The event payload: ${payload}`);
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const { LinearClient } = require('@linear/sdk')
-const linear = new LinearClient({
+/*const linear = new LinearClient({
     apiKey: "lin_api_2IczXFvoXNVm9LeXPNyo7DcQOtOaZ7eusoQuT2OO"
-});
+});*/
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function getMyIssues() {
-    //const issues = await linear.issues()
-    //console.log(issues)
+    /*const issues = await linear.issues()
+    console.log(issues.issueSearch("CHA-921"))*/
+    console.log(process.env.LINEAR_API_KEY)
+    console.log(process.env)
 }
 
 try {
     getMyIssues()
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    //const payload = JSON.stringify(github.context.payload, undefined, 2)
+    //const time = (new Date()).toTimeString();
+    //core.setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
-    console.log(`The event payload: ${payload}`);
+    //console.log(`The event payload: ${payload}`);
 } catch (error) {
     core.setFailed(error.message);
 }
